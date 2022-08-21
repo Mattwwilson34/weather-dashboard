7aeefac131b5a218a9338cd2022886ac4e8f4bff
Matt Wilson <mattwwilson34@gmail.com>
2022-08-21 17:40:52 -0400

    Cut/Add: cut tomorrow.io API, add openWeather API

--- backend/get-weather-data.js
+++ backend/get-weather-data.js
@@ -1,67 +1,26 @@
 import axios from 'axios';
-import queryString from 'query-string';
-import moment from 'moment';
 import { API_KEY } from './global-env-variables.js';
 
 const getWeatherData = async () => {
-  // set the Timelines GET endpoint as the target URL
-  const getTimelineURL = 'https://api.tomorrow.io/v4/timelines';
+  // set the openWeather endpoint as the target URL
+  const openWeatherURL = 'https://api.openweathermap.org/data/2.5/forecast';
 
   // get your key from app.tomorrow.io/development/keys
   const apikey = API_KEY;
 
   // pick the location, as a latlong pair
-  let location = [40.758, -73.9855];
+  let lat = 35.994;
+  let lon = -78.898;
 
-  // list the fields
-  const fields = [
-    'precipitationIntensity',
-    'precipitationType',
-    'windSpeed',
-    'windGust',
-    'windDirection',
-    'temperature',
-    'temperatureApparent',
-    'cloudCover',
-    'cloudBase',
-    'cloudCeiling',
-    'weatherCode',
-  ];
-
-  // choose the unit system, either metric or imperial
+  // choose the unit system
   const units = 'imperial';
-
-  // set the timesteps, like "current", "1h" and "1d"
-  const timesteps = ['current', '1h', '1d'];
-
-  // configure the time frame up to 6 hours back and 15 days out
-  const now = moment.utc();
-  const startTime = moment.utc(now).add(0, 'minutes').toISOString();
-  const endTime = moment.utc(now).add(1, 'days').toISOString();
-
-  // specify the timezone, using standard IANA timezone format
-  const timezone = 'America/New_York';
-
-  // request the timelines with all the query string parameters as options
-  const getTimelineParameters = queryString.stringify(
-    {
-      apikey,
-      location,
-      fields,
-      units,
-      timesteps,
-      startTime,
-      endTime,
-      timezone,
-    },
-    { arrayFormat: 'comma' },
-  );
 
   try {
     const weatherData = await axios.get(
-      `${getTimelineURL}?${getTimelineParameters}`,
+      `${openWeatherURL}?lat=${lat}&lon=${lon}&appid=${apikey}&units=${units}`,
     );
-    return weatherData.data.data;
+    console.log(weatherData);
+    return weatherData.data;
   } catch (error) {
     console.error(error);
   }
