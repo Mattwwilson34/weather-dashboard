985ec2a3f4865ff0baae6f98d31c5a3f3d69901f
Matt Wilson <mattwwilson34@gmail.com>
2022-08-26 18:14:38 -0400

    Add: updated routes to hit seperate API's

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import logger from 'morgan';
+import getCurrentWeatherData from './get-current-weather-data.js';
 import getForcastWeatherData from './get-forcast-weather-data.js';
 
 const port = 3000;
@@ -11,7 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/', async (req, res) => {
+app.get('/current', async (req, res) => {
+  try {
+    const weatherData = await getCurrentWeatherData();
+    res.send(weatherData);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get('/forcast', async (req, res) => {
   try {
     const weatherData = await getForcastWeatherData();
     res.send(weatherData);
