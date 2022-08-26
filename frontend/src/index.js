7c2ad7cbd3b42ce6d7dc6f3e122c337ee3ed51b9
Matt Wilson <mattwwilson34@gmail.com>
2022-08-26 18:46:21 -0400

    Add: turn off eslint unused vars on import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import getCurrentWeatherData from './modules/get-current-weather-data.js';
-import getForcastWeatherData from './modules/get-forcast-weather-data.js';
 import moment from 'moment';
 import css from './styles/style.css';
+/* eslint-disable no-unused-vars */
 
 // Added to prevent API call on app refresh
 const weatherButton = document.querySelector('button');
