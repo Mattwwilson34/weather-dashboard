63892b95c3c19e3ba47ff79d0a3bb033d006e825
Matt Wilson <mattwwilson34@gmail.com>
2022-08-26 18:14:22 -0400

    Add: current weather API call func

---
+++ backend/get-current-weather-data.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { API_KEY } from './global-env-variables.js';
+
+const getCurrentWeatherData = async () => {
+  // set the openWeather endpoint as the target URL
+  const openWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+  // get your key from app.tomorrow.io/development/keys
+  const apikey = API_KEY;
+
+  // pick the location, as a latlong pair
+  let lat = 35.994;
+  let lon = -78.898;
+
+  // choose the unit system
+  const units = 'imperial';
+
+  try {
+    const weatherData = await axios.get(
+      `${openWeatherURL}?lat=${lat}&lon=${lon}&appid=${apikey}&units=${units}`,
+    );
+    return weatherData.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default getCurrentWeatherData;
