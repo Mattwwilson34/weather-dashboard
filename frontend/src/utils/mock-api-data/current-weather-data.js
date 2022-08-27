5ebdfba9928e0daa39ae65a27be01a5a1412be3b
Matt Wilson <mattwwilson34@gmail.com>
2022-08-27 11:45:06 -0400

    Add: mock API call data for testing

---
+++ frontend/src/utils/mock-api-data/current-weather-data.js
@@ -0,0 +1,45 @@
+const MOCK_CURRENT_WEATHER_API_DATA = {
+  coord: {
+    lon: -78.9,
+    lat: 35.99,
+  },
+  weather: [
+    {
+      id: 802,
+      main: 'Clouds',
+      description: 'scattered clouds',
+      icon: '03d',
+    },
+  ],
+  base: 'stations',
+  main: {
+    temp: 81.34,
+    feels_like: 86.59,
+    temp_min: 77.09,
+    temp_max: 83.98,
+    pressure: 1021,
+    humidity: 77,
+  },
+  visibility: 10000,
+  wind: {
+    speed: 0,
+    deg: 0,
+  },
+  clouds: {
+    all: 40,
+  },
+  dt: 1661608225,
+  sys: {
+    type: 2,
+    id: 2040014,
+    country: 'US',
+    sunrise: 1661597021,
+    sunset: 1661644259,
+  },
+  timezone: -14400,
+  id: 4464368,
+  name: 'Durham',
+  cod: 200,
+};
+
+export default MOCK_CURRENT_WEATHER_API_DATA;
