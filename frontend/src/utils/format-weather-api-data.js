76354938753e471c3544bf4cc8a0f4b3e35df4e3
Matt Wilson <mattwwilson34@gmail.com>
2022-10-04 11:44:41 -0400

    Add: format weather data function

---
+++ frontend/src/utils/format-weather-api-data.js
@@ -0,0 +1,27 @@
+import { default as weatherData } from './mock-api-data/forcast-weather-data.js';
+import moment from 'moment';
+
+const formatTimeStamps = (weatherData) => {
+  //
+  // Deep copy API data
+  const weatherDataClone = JSON.parse(JSON.stringify(weatherData));
+
+  // Format weather time stamps
+  weatherDataClone.list.forEach((timeStamp) => {
+    timeStamp.dt = moment
+      .unix(timeStamp.dt)
+      .format('dddd, MMMM Do YYYY, h:mm:ss a');
+  });
+
+  // Format sunrise/set
+  weatherDataClone.city.sunrise = moment
+    .unix(weatherDataClone.city.sunrise)
+    .format('h:mm:ss a');
+  weatherDataClone.city.sunset = moment
+    .unix(weatherDataClone.city.sunset)
+    .format('h:mm:ss a');
+
+  return weatherDataClone;
+};
+
+export default formatTimeStamps;
