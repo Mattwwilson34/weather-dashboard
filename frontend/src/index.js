4779cfd0b26373d88d75e04ff40b0d3ca38a5aa2
Matt Wilson <mattwwilson34@gmail.com>
2022-10-12 17:25:08 -0400

    Add: random zipcode generator + modularize events

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@
 import getWeatherDataFromZip from './modules/get-weather-data-from-zip.js';
 import formatTimeStamps from './utils/format-weather-api-data.js';
 import reduceWeatherTimeStamps from './utils/reduce-weather-timeStamps.js';
+import getRandomZipCode from './utils/random-zipcode-generator.js';
 import css from './styles/style.css';
 /* eslint-disable no-unused-vars */
 
@@ -11,33 +12,45 @@ const app = {
   async init() {
     //
     // Fetch API data
-    await app.getData(SEED_ZIP_CODE);
-    app.storeDomElements();
-    app.populateData(this.reducedWeatherData);
+    await this.getData(SEED_ZIP_CODE);
+    this.storeDomElements();
+    this.populateData(this.reducedWeatherData);
+    this.bindEventListeners();
+  },
 
-    // Add event listener for zipcode search
-    const searchBtn = document.querySelector('.search-btn');
-    searchBtn.addEventListener('click', async (event) => {
-      //
-      // store zipcode input value
-      const zipCode = app.locationInput.value;
+  bindEventListeners() {
+    this.searchBtn.addEventListener('click', this.search.bind(this));
+    this.randomBtn.addEventListener('click', this.randomWeather.bind(this));
+  },
 
-      // handle incorrect user input that would break API call
-      if (isNaN(parseInt(zipCode)) || zipCode.length !== 5) {
-        alert('Please enter a valid 5 digit zip code (ex: 19054).');
-        app.locationInput.value = '';
-        return;
-      }
+  async search() {
+    //
+    // store zipcode input value
+    const zipCode = this.locationInput.value;
 
-      await app.getData(app.locationInput.value);
-      app.populateData(app.reducedWeatherData);
-    });
+    // handle incorrect user input that would break API call
+    if (isNaN(parseInt(zipCode)) || zipCode.length !== 5) {
+      alert('Please enter a valid 5 digit zip code (ex: 19054).');
+      this.locationInput.value = '';
+      return;
+    }
+
+    await this.getData(zipCode);
+    this.populateData(this.reducedWeatherData);
+  },
+
+  async randomWeather() {
+    const zipCode = getRandomZipCode();
+    await this.getData(zipCode);
+    this.populateData(this.reducedWeatherData);
   },
 
   storeDomElements() {
     // DOM Elem
     this.location = document.querySelector('.location');
     this.locationInput = document.querySelector('.location-input > input');
+    this.searchBtn = document.querySelector('.search-btn');
+    this.randomBtn = document.querySelector('.random-btn');
     this.weatherCards = document.querySelectorAll('.weather-card');
     this.dayOfWeek = document.querySelectorAll('.weather-card > h3');
     this.weatherIcons = document.querySelectorAll('.weather-card > img');
