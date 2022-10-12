1552e356c82aa983b9449e56ab70bff049a9c8c5
Matt Wilson <mattwwilson34@gmail.com>
2022-10-12 16:41:12 -0400

    Add: modularize app logic
    To improve readability and code reusablility I broke the app logic into
    modules within the app object.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,47 +2,95 @@
 import getWeatherDataFromZip from './modules/get-weather-data-from-zip.js';
 import formatTimeStamps from './utils/format-weather-api-data.js';
 import reduceWeatherTimeStamps from './utils/reduce-weather-timeStamps.js';
-import moment from 'moment';
 import css from './styles/style.css';
 /* eslint-disable no-unused-vars */
 
 const SEED_ZIP_CODE = 27703;
 
-// DOM Elem
-const weatherCards = document.querySelectorAll('.weather-card');
-const dayOfWeek = document.querySelectorAll('.weather-card > h3');
-const weatherIcons = document.querySelectorAll('.weather-card > img');
-const weatherDataDescriptions = document.querySelectorAll(
-  '.weather-description',
-);
-const weatherDataDivs = document.querySelectorAll('.weather-data');
-const weatherTimeStamps = document.querySelectorAll('.time-stamp');
-
-const iconBaseURL = 'http://openweathermap.org/img/wn/';
-
 const app = {
   async init() {
-    const weatherData = await getWeatherDataFromZip(SEED_ZIP_CODE);
-    const formatedWeatherData = formatTimeStamps(weatherData.data);
-    const reducedWeatherData = reduceWeatherTimeStamps(formatedWeatherData, 5);
-    console.log(formatedWeatherData);
-    console.log(reducedWeatherData);
+    //
+    // Fetch API data
+    await app.getData(SEED_ZIP_CODE);
+    app.storeDomElements();
+    app.populateData(this.reducedWeatherData);
 
-    weatherCards.forEach((card, i) => {
+    // Add event listener for zipcode search
+    const searchBtn = document.querySelector('.search-btn');
+    searchBtn.addEventListener('click', async (event) => {
       //
-      dayOfWeek[i].textContent = reducedWeatherData[i].dt.split(',')[0];
+      // store zipcode input value
+      const zipCode = app.locationInput.value;
+
+      // handle incorrect user input that would break API call
+      if (isNaN(parseInt(zipCode)) || zipCode.length !== 5) {
+        alert('Please enter a valid 5 digit zip code (ex: 19054).');
+        app.locationInput.value = '';
+        return;
+      }
+
+      await app.getData(app.locationInput.value);
+      app.populateData(app.reducedWeatherData);
+    });
+  },
+
+  storeDomElements() {
+    // DOM Elem
+    this.location = document.querySelector('.location');
+    this.locationInput = document.querySelector('.location-input > input');
+    this.weatherCards = document.querySelectorAll('.weather-card');
+    this.dayOfWeek = document.querySelectorAll('.weather-card > h3');
+    this.weatherIcons = document.querySelectorAll('.weather-card > img');
+    this.weatherDataDescriptions = document.querySelectorAll(
+      '.weather-description',
+    );
+    this.weatherDataDivs = document.querySelectorAll('.weather-data');
+    this.weatherTimeStamps = document.querySelectorAll('.time-stamp');
+
+    this.iconBaseURL = 'http://openweathermap.org/img/wn/';
+  },
+
+  async getData(zipCode) {
+    // Fetch API data
+    this.weatherData = await getWeatherDataFromZip(zipCode);
+
+    console.log(this.weatherData);
+    const formatedWeatherData = formatTimeStamps(
+      this.weatherData.forcastWeatherData.data,
+    );
+    this.reducedWeatherData = reduceWeatherTimeStamps(formatedWeatherData, 5);
+    console.log(formatedWeatherData);
+    console.log(this.reducedWeatherData);
+  },
+
+  populateData(reducedWeatherData) {
+    //
+    // Update location
+    const { city, country, state } =
+      this.weatherData.zipCodeData.data.location[0];
+    this.location.textContent = `${city} ${state}, ${country}`;
+
+    // Fill weather cards with API data
+    this.weatherCards.forEach((card, i) => {
+      //
+      this.dayOfWeek[i].textContent = reducedWeatherData[i].dt.split(',')[0];
 
       const iconCode = reducedWeatherData[i].weather[0].icon;
-      weatherIcons[i].src = `${iconBaseURL}${iconCode}@2x.png`;
+      this.weatherIcons[i].src = `${this.iconBaseURL}${iconCode}@2x.png`;
 
-      weatherDataDivs[i].textContent = Math.round(
+      this.weatherDataDivs[i].textContent = Math.round(
         reducedWeatherData[i].main.temp,
       );
 
-      weatherDataDescriptions[i].textContent =
+      this.weatherDataDescriptions[i].textContent =
         reducedWeatherData[i].weather[0].description;
 
-      weatherTimeStamps[i].textContent = reducedWeatherData[i].dt.split(',')[2];
+      // format time
+      const formatedTime = reducedWeatherData[i].dt.split(',')[2].split(':');
+
+      this.weatherTimeStamps[
+        i
+      ].textContent = `${formatedTime[0]}:${formatedTime[2]}`;
     });
   },
 };
