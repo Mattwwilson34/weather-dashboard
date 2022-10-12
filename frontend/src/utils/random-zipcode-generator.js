7a7644916dc9409dca8119e12525963a5cec4a50
Matt Wilson <mattwwilson34@gmail.com>
2022-10-12 17:24:42 -0400

    Add: randomZipcode generator

---
+++ frontend/src/utils/random-zipcode-generator.js
@@ -0,0 +1,14 @@
+import zipcodeData from './zipcode-data/USCities.json';
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const getRandomZipCode = () => {
+  const randomIndex = getRandomIntInclusive(0, zipcodeData.length - 1);
+  return zipcodeData[randomIndex].zip_code;
+};
+
+export default getRandomZipCode;
